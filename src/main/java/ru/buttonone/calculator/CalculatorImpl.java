eb5e3ba7ae278524cf7010e6b2e4505c899994a3
Pavel <p666v@yandex.ru>
2024-01-20 17:12:05 +0300

    Добавил проброс Исключения

--- src/main/java/ru/buttonone/calculator/CalculatorImpl.java
+++ src/main/java/ru/buttonone/calculator/CalculatorImpl.java
@@ -18,6 +18,9 @@ public class CalculatorImpl implements Calculator  {
 
     @Override
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
         return a / b;
     }
 }
